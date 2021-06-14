86c66b02aaec1dcbd8e5be4dfbbbc991453bceda
Denys Kravchuk <denis.kravchuk.ua@gmail.com>
2021-06-14 12:04:07 +0300

    added bitrix online chat

--- wp-content/themes/onepress/footer.php
+++ wp-content/themes/onepress/footer.php
@@ -55,7 +55,14 @@ if ( ! $hide_footer ) {
 			</div>
 		</div>
 		<!-- .site-info -->
-
+	
+		<script>
+        (function(w,d,u){
+                var s=d.createElement('script');s.async=true;s.src=u+'?'+(Date.now()/60000|0);
+                var h=d.getElementsByTagName('script')[0];h.parentNode.insertBefore(s,h);
+        })(window,document,'https://cdn.bitrix24.ua/b17815417/crm/site_button/loader_2_xjdans.js');
+</script>
+		
 	</footer><!-- #colophon -->
 	<?php
 }
